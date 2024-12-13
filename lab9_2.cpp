008c41a61d0c7b46cd2cea32ac716b120f7dbc45
Chinoros <chinoros2815@gmail.com>
2024-12-13 15:32:05 +0700

    edited

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N, int M) {
+	if (N <= 0 || M <= 0) {cout << "Invalid input"; return;}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << 'O';
+		}
+		cout << '\n';
+	}
+}
 
 int main(){
 	
